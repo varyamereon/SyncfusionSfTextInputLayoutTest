03500167b6f344b9b5ff718a32ae607e9c729e15
Will Beal <willbeal@outlook.com>
2021-06-15 12:11:31 +0200

    Add project files.

---
+++ SyncfusionSfTextInputLayoutTest/SyncfusionSfTextInputLayoutTest/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("D-DIN.otf", Alias = "D-DIN")]
+[assembly: ExportFont("D-DINCondensed.otf", Alias = "D-DIN Condensed")]
+[assembly: ExportFont("D-DINCondensed-Bold.otf", Alias = "D-DIN Condensed Bold")]
